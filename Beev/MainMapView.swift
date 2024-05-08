23752b115f313f09446e9b568a1f2b754317883f
mariaconcettarena <mariaconcettaarena@hotmail.it>
2024-05-08 23:16:15 +0200

    modifica mainmap

--- Beev/MainMapView.swift
+++ Beev/MainMapView.swift
@@ -1,25 +1,52 @@
-
-
 import SwiftUI
 import MapKit
 import CoreLocation
 
+
 // ContentView
 struct MainMapView: View {
     
+    @State private var myPosition: MKCoordinateRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 0, longitude: 90),
+        span: MKCoordinateSpan(latitudeDelta: 180, longitudeDelta: 180)
+    )
     
-    @State private var myPosition: MapCameraPosition = .userLocation(fallback: .automatic)
-   
-    
+    @State private var style: MKMapType = .hybridFlyover
     
     var body: some View {
-        Map(position: $myPosition) {
+        Map(coordinateRegion: $myPosition)
+        
+        
+            .onAppear {
             
-        }.onAppear{CLLocationManager().requestWhenInUseAuthorization()} //POSIZIONE
-            .mapControls{
-                MapUserLocationButton() //BOTTONE PER TORNARE ALLA MIA POSIZIONE
-                MapPitchToggle() //BOTTONE PER PASSARE AL 3D
+                
+                // Set the initial map region
+                myPosition = MKCoordinateRegion(
+                    center: CLLocationCoordinate2D(latitude: 0, longitude: 90),
+                    span: MKCoordinateSpan(latitudeDelta: 180, longitudeDelta: 180)
+                )
+                // Request location authorization
+                CLLocationManager().requestWhenInUseAuthorization()
+                
+                
             }
+            .mapControls {
+                MapUserLocationButton() // Button to return to user's location
+                MapPitchToggle() // Button to switch to 3D
+            }
+        
+    }
+}
+
+
+
+
+
+
+//PREVIEW
+struct MainMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMapView()
     }
 }
 
