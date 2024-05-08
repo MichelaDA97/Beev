c9dc499cc7b72f64b1aeab46888e3ad2994ed1ad
Michela D'Avino <micheladavino.mda@gmail.com>
2024-05-08 13:57:23 +0200

    Initial Commit

---
+++ Beev/BeevApp.swift
@@ -0,0 +1,20 @@
+//
+//  BeevApp.swift
+//  Beev
+//
+//  Created by Michela D'Avino on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BeevApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
